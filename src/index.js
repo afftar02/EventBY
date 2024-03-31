592b08444e63b663309f2cfeb36485a94063ce46
Nikita <NStetskiy@alfabank.ru>
2024-03-31 20:05:13 +0300

    Redux store initialized

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import ErrorBoundary from 'components/ErrorBoundary';
 import ErrorFallback from 'components/ErrorFallback';
 
+import { store } from './redux/store';
 import App from './App';
 
 import './index.css';
@@ -12,8 +14,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <ErrorBoundary fallback={<ErrorFallback />}>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </ErrorBoundary>
 );
